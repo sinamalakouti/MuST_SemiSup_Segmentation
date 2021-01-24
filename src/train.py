c60e2aa032c3c0c088d8f08d6c68b917c5303e00
sina malakouti <siinamalakouti@gmail.com>
2021-01-24 02:01:58 -0500

    Update train.py

--- src/train.py
+++ src/train.py
@@ -590,7 +590,7 @@ def train_with_fcm(dataset):
 
                 plt.imshow(intermediate_pred.cpu().reshape((212, 256)))
                 plt.savefig("../images/segmentation/iter_{}/linear_comb_{}.png".format(iter, iter))
-                utils.evaluate("../images/segmentation/iter_{}/")
+                utils.evaluate(dataset,wnet,"../images/segmentation/iter_{}")
 
         # torch.autograd.set_detect_anomaly(True)
         wnet.train()
@@ -624,7 +624,7 @@ def train_with_fcm(dataset):
             if iter > 100:
                 alpha = alpha / 100
             fcm_loss = alpha * fcm_loss
-            final_loss = recon_loss + intermediate_recon_loss + fcm_loss 
+            final_loss = recon_loss + intermediate_recon_loss + fcm_loss
 
             final_loss.backward()
             optimizer.step()
