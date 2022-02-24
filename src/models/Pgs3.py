585ca009058dcbd6205c3a018c82ee21445834e6
sina malakouti <siinamalakouti@gmail.com>
2022-02-24 17:10:27 -0500

    Update Pgs3.py

--- src/models/Pgs3.py
+++ src/models/Pgs3.py
@@ -283,6 +283,88 @@ class PGS_Independent(nn.Module):
         unsupervised_outputs = output5_stud, output6_stud, output7_stud, output8_stud, output9_stud
         return supervised_outputs, unsupervised_outputs
 
+
+    def __fw_unsupervised_independent_decoders(self, X):  # only_feature space aug
+        # no detach
+        # contracting path ( SHARED ENCODER )
+        c1, d1, c2, d2, c3, d3, c4, d4 = self.__fw_contracting_path(X)
+
+        # teacher outputs
+        with torch.no_grad():
+            c5_teach = self.__fw_bottleneck(d4)
+            aug_output5_teach = self.cls5_teach(c5_teach)
+            teach_up1 = self.__fw_up(c5_teach, c4, self.up1_teach,
+                                     transformer=None) if self.config.information_passing_strategy == 'teacher' \
+                else self.__fw_up(c5_teach, c4, self.up1_teach, transformer=None)
+
+            c6_teach = self.__fw_expand_4layer(teach_up1)
+            aug_output6_teach = self.cls6_teach(c6_teach)
+
+            teach_up2 = self.__fw_up(c6_teach, c3, self.up2_teach,
+                                     transformer=None) if self.config.information_passing_strategy == 'teacher' \
+                else self.__fw_up(c6_teach, c3, self.up2_teach, transformer=None)
+
+            c7_teach = self.__fw_expand_3layer(teach_up2)
+            aug_output7_teach = self.cls7_teach(c7_teach)
+
+            teach_up3 = self.__fw_up(c7_teach, c2, self.up3_teach,
+                                     transformer=None) if self.config.information_passing_strategy == 'teacher' \
+                else self.__fw_up(c7_teach, c2, self.up3_teach, transformer=None)
+
+            c8_teach = self.__fw_expand_2layer(teach_up3)
+            aug_output8_teach = self.cls8_teach(c8_teach)
+
+            teach_up4 = self.__fw_up(c8_teach, c1, self.up4_teach,
+                                     transformer=None) if self.config.information_passing_strategy == 'teacher' \
+                else self.__fw_up(c8_teach, c1, self.up4_teach, transformer=None)
+
+            c9_teach = self.__fw_expand_1layer(teach_up4)
+            aug_output9_teach = self.cls9_teach(c9_teach)
+
+        # students
+
+        d4_stud, _ = self.transformer(d4, None, perturbation_mode='F')
+        c5_stud = self.__fw_bottleneck(d4_stud, False)
+        output5_stud = self.cls5_stud(c5_stud)
+
+        stud_up1 = self.__fw_up(c5_stud, c4, self.up1_stud,
+                                transformer=self.transformer) if self.config.information_passing_strategy == 'teacher' \
+            else self.__fw_up(c5_stud, c4, self.up1_stud, transformer=self.transformer)
+
+        c6_stud = self.__fw_expand_4layer(stud_up1, False)
+        output6_stud = self.cls6_stud(c6_stud)
+        ######
+
+        stud_up2 = self.__fw_up(c6_stud, c3, self.up2_stud,
+                                transformer=self.transformer) if self.config.information_passing_strategy == 'teacher' \
+            else self.__fw_up(c6_stud, c3, self.up2_stud, transformer=self.transformer)
+
+        c7_stud = self.__fw_expand_3layer(stud_up2, False)
+        output7_stud = self.cls7_stud(c7_stud)
+
+        #####
+
+        stud_up3 = self.__fw_up(c7_stud, c2, self.up3_stud,
+                                transformer=self.transformer) if self.config.information_passing_strategy == 'teacher' \
+            else self.__fw_up(c7_stud, c2, self.up3_stud, transformer=self.transformer)
+
+        c8_stud = self.__fw_expand_2layer(stud_up3, False)
+        output8_stud = self.cls8_stud(c8_stud)
+
+        ####
+
+        stud_up4 = self.__fw_up(c8_stud, c1, self.up4_stud,
+                                transformer=self.transformer) if self.config.information_passing_strategy == 'teacher' \
+            else self.__fw_up(c8_stud, c1, self.up4_stud, transformer=self.transformer)
+
+        # output9 is the main output of the network
+
+        c9_stud = self.__fw_expand_1layer(stud_up4, False)
+        output9_stud = self.cls9_stud(c9_stud)
+
+        supervised_outputs = aug_output5_teach, aug_output6_teach, aug_output7_teach, aug_output8_teach, aug_output9_teach
+        unsupervised_outputs = output5_stud, output6_stud, output7_stud, output8_stud, output9_stud
+        return supervised_outputs, unsupervised_outputs
     def __fw_supervised(self, X):
 
         c1, d1, c2, d2, c3, d3, c4, d4 = self.__fw_contracting_path(X)
